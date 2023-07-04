4342ed98028342059d5d0263a10cab8c390eb081
ShondreGrobler <shondre@idealsolution.co.za>
2023-07-04 16:37:09 +0200

    Initial Project Add

---
+++ pft-pet-clinic/src/main/java/shondre/springframework/pftpetclinic/PftPetClinicApplication.java
@@ -0,0 +1,13 @@
+package shondre.springframework.pftpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PftPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PftPetClinicApplication.class, args);
+    }
+
+}
---
+++ pft-pet-clinic/src/test/java/shondre/springframework/pftpetclinic/PftPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package shondre.springframework.pftpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PftPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
